0f2e6b6ed7e1607fbab605f4f8765a1b29b4aad9
Enuch <50813054+Enuch@users.noreply.github.com>
2020-07-16 20:14:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -19,12 +19,13 @@ int main()
     };
   };
 
-  int qtdDepartamentos, i = 0, opcao = -1, nLojas = 0, opcao1 = 1;
+  int qtdDepartamentos, i = 0, opcao = -1, nLojas = 0, opcao1 = 1, CNPJaux = -5;
+  bool bol = false, bol2 = false;
 
   Lojas store[100];
 
   while(opcao != 0){
-
+  system("cls");
   cout << "Escolha uma opcao!" << endl;
   cout << "1 - Lojas" << endl;
   cout << "2 - Registrar Loja" << endl;
@@ -40,19 +41,55 @@ int main()
       {
         cout << "Nao ha lojas registradas no sistema! \nCaso deseje registrar escolha a opcao no menu principal" << endl;
       }
+      else{
+        cout << "Escolha uma loja para acessar seus departamentos: " << endl;
+        for (i = 0; i < nLojas; i++)
+        {
+          cout << i << " - "<< store[i].nome << endl;
+
+        }
+        
+      }
 
       break;
     case 2:
       while (opcao1 != 0){
+        nLojas++;
       
     cout << "Digite o nome da loja a ser registrada: " << i + 1 << endl;
     getchar();
     getline(cin, store[i].nome);
-    cout << "Digite o CNPJ da loja registrada: " << i + 1 << endl;
-    cin >> store[i].CNPJ;
+    cout << "Digite o CNPJ da loja: " << i + 1 << endl;
+    cin >> CNPJaux;
     getchar();
+    while(bol2 == false)
+    {
+      for (int j = 0; j < nLojas; j++)
+      {
+        if (store[j].CNPJ == CNPJaux)
+        {
+          bol = true;
+        }
+      }
+      if(bol == true)
+      {
+        bol = false;
+        cout << "'CNPJ' já cadastrado \nPor favor digite outro: " << endl;
+        cout << "Digite o CNPJ da loja: " << i + 1 << endl;
+        cin >> CNPJaux;
+      }
+      else
+      {
+        
+        bol2 = true;
+        store[i].CNPJ = CNPJaux;
+        cout << bol << endl;
+      }
+    }
+    bol2 = false;
+
     i++;
-    nLojas++;
+    
     cout << "Caso deseje parar de registrar digite '0': ";
     cin >> opcao1;
     }
